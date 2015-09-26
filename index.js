76752b2451356852fcf866392afff1650e275dd2
Andrew Sardone <andrew@andrewsardone.com>
2015-09-26 17:15:07 -0400

    Stub in store & item data for Item component
    Per the demo:
    
      https://medium.com/@clayallsopp/relay-101-building-a-hacker-news-client-bb8b2bdc76e6

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ const ReactDOM = require('react-dom');
 
 class Item extends React.Component {
   render() {
+    const item = this.props.store.item;
+
     return (
-      <h1>Hello world</h1>
+      <div>
+        <h1><a href={item.url}>{item.title}</a></h1>
+        <h2>{item.score} – {item.by.id}</h2>
+      </div>
     );
   }
 }
 
+const store = {
+  item: {
+    id: '1337',
+    url: 'http://google.com',
+    title: 'Google',
+    score: 100,
+    by: {
+      id: 'clay'
+    }
+  }
+};
+
 const mountNode = document.getElementById('container');
-const rootComponent = <Item />
+const rootComponent = <Item store={store} />
 ReactDOM.render(rootComponent, mountNode);
