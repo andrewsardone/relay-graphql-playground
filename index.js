cb6e2bf94b49c78fe61707920a6563ff4e30cfe6
Andrew Sardone <andrew@andrewsardone.com>
2015-09-26 17:10:50 -0400

    Stub simple Item React component

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert("hello relay!");
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class Item extends React.Component {
+  render() {
+    return (
+      <h1>Hello world</h1>
+    );
+  }
+}
+
+const mountNode = document.getElementById('container');
+const rootComponent = <Item />
+ReactDOM.render(rootComponent, mountNode);
